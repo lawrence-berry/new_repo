8f5a6145ad382956b9211d95edb0b02c9b8e5e6c
Lawrence Berry <lawrence.berry@gmail.com>
2020-07-22 21:20:16 +0100

    Add basic bash script support

--- new_repo.sh
+++ new_repo.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+# $1 is a relative path from $HOME/Projects to install to
+# $2 is one of api, fe (both geo.me), bash
 if [[ ! $1 =~ / ]]; then
   echo "Please include containing folder, e.g.:"
   echo "  new_repo util/app"
   exit 1
 fi
 
+TEMPLATE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_FOLDERS_LOCATION="/Users/$USER/Projects/"
 FOLDER_NAME=`echo $1 | cut -d "/" -f 1`
 PROJECT_NAME=`echo $1 | cut -d "/" -f 2`
 PROJECT_PATH=$PROJECT_FOLDERS_LOCATION$1
 
-if [[ $FOLDER_NAME =~ ^geome$ && $PROJECT_NAME =~ api$ || $2 =~ api ]]; then
-  echo "yes"
+if [[ $FOLDER_NAME =~ ^geome$ && $PROJECT_NAME =~ api$ || $2 =~ api|fe ]]; then
   cd /Users/$USER/Projects/geome/template
   /Users/$USER/Projects/geome/template/bin/setup
   cd $PROJECT_PATH
 else
   mkdir -p ~/Projects/$1
+  if [[ $2 =~ bash|ruby ]]; then
+    cp $TEMPLATE_DIR/run.sh $TEMPLATE_DIR/install.sh ~/Projects/$1
+  fi
   cd ~/Projects/$1
   git init .
   touch README.md
